a3e64d095591bf9f3b90fb170d5067e2247af3e0
Dushyanth <kd13@iitbbs.ac.in>
2021-04-18 19:19:01 +0530

    profile register grades

---
+++ student/register/register.js
@@ -0,0 +1,365 @@
+(function(){
+    let data ={
+        "coursesList": [
+            {
+                "sroll": "18CS01001",
+                "course_id": 1,
+                "cname": "PDS",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 2,
+                "cname": "ENGLISH",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 3,
+                "cname": "PHYSICS",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 4,
+                "cname": "MATHS",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 5,
+                "cname": "MATHS2",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 6,
+                "cname": "CHEMISTRY",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 7,
+                "cname": "ENGLISH2",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 16,
+                "cname": "INTRO TO ML",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 4,
+                "credits": 1
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 8,
+                "cname": "CHEMISTRY LAB",
+                "ctype": "LAB",
+                "dept_id": 3,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 9,
+                "cname": "DATA STRUCTURES",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 3,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 10,
+                "cname": "DISCRETE MATHS",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 3,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 11,
+                "cname": "DATA STRUCTURES LAB",
+                "ctype": "LAB",
+                "dept_id": 1,
+                "semno": 3,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 12,
+                "cname": "MATHS3",
+                "ctype": "CORE",
+                "dept_id": 3,
+                "semno": 3,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 13,
+                "cname": "COMBINATORICS",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 4,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 14,
+                "cname": "ALGORITHMS",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 4,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 15,
+                "cname": "ALGORITHMS LAB",
+                "ctype": "LAB",
+                "dept_id": 1,
+                "semno": 4,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 17,
+                "cname": "OPERATING SYSTEMS",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 5,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 18,
+                "cname": "COA",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 5,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 19,
+                "cname": "OPERATING SYSTEMS LAB",
+                "ctype": "LAB",
+                "dept_id": 1,
+                "semno": 5,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 20,
+                "cname": "COA LAB",
+                "ctype": "LAB",
+                "dept_id": 1,
+                "semno": 5,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 21,
+                "cname": "DATABASE SYSTEMS",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 6,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 22,
+                "cname": "DATABASE SYSTEMS LAB",
+                "ctype": "LAB",
+                "dept_id": 1,
+                "semno": 6,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 23,
+                "cname": "CN",
+                "ctype": "CORE",
+                "dept_id": 1,
+                "semno": 6,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "course_id": 24,
+                "cname": "CN LAB",
+                "ctype": "LAB",
+                "dept_id": 1,
+                "semno": 6,
+                "credits": 3
+            }
+        ],
+        "registered": false,
+        "offeredCourses": [
+            {
+                "course_id": 21,
+                "cname": "DATABASE SYSTEMS",
+                "ctype": "CORE",
+                "dept_id": 0,
+                "semno": 6,
+                "credits": 4
+            },
+            {
+                "course_id": 22,
+                "cname": "DATABASE SYSTEMS LAB",
+                "ctype": "LAB",
+                "dept_id": 0,
+                "semno": 6,
+                "credits": 3
+            },
+            {
+                "course_id": 23,
+                "cname": "CN",
+                "ctype": "CORE",
+                "dept_id": 0,
+                "semno": 6,
+                "credits": 4
+            },
+            {
+                "course_id": 24,
+                "cname": "CN LAB",
+                "ctype": "LAB",
+                "dept_id": 0,
+                "semno": 6,
+                "credits": 3
+            }
+        ]
+    }
+    /*
+            "sroll": "18CS01001",
+            "course_id": 2,
+            "cname": "ENGLISH",
+            "ctype": "CORE",
+            "dept_id": 3,
+            "semno": 1,
+            "credits": 4
+    */
+   let coursesList = data.coursesList;
+   let registered = data.registered;
+   let offeredCourses = data.offeredCourses;
+
+   let lc = document.getElementById("lcourses");
+   
+   let sem_wise=[];
+   for(let i=0;i<8;i++){
+       sem_wise.push([]);
+   }
+   for(let i=0;i<coursesList.length;i++){
+       sem_wise[coursesList[i].semno-1].push(coursesList[i]);
+   }
+
+   //console.log(sem_wise);
+    if(registered==false){
+       /* {
+            "course_id": 21,
+            "cname": "DATABASE SYSTEMS",
+            "ctype": "CORE",
+            "dept_id": 0,
+            "semno": 6,
+            "credits": 4
+        }*/
+        let k = document.getElementById("reg");
+        let x=`
+                <h6 class="text-center bg-dark text-white border border-dark p-2">Register</h6>
+                </hr>
+
+                <table class="w-75 mx-auto">
+                    <tr>
+                        <th>Course Name</th>
+                        <th>Course Type</th>
+                        <th>Credits</th>
+                        <th>Register</th>
+                    </tr>`
+        for(let i=0;i<offeredCourses.length;i++)
+        {
+                x+=`
+                <tr>
+                    <td>
+                        ${offeredCourses[i].cname}
+                    </td>
+                    <td>
+                        ${offeredCourses[i].ctype}
+                    </td>
+                    <td>
+                        ${offeredCourses[i].credits}
+                    </td>
+                    <td><input type="checkbox" value="" id=${offeredCourses[i].course_id}></td>
+                </tr>
+                `
+        }
+        x+=`
+        </table>`;
+        x+=`
+           <div class="mt-2 d-flex justify-content-center"> <button type="submit" class="btn btn-dark">Submit</button></div>
+        `;
+        k.innerHTML = x;
+    }
+
+   let globalList =``;
+   for(let i=0;i<8;i++){
+       if(sem_wise[i].length!=0){
+            let x =`
+            <table class="w-100 mb-3">
+                <tr>
+                <th class="xyz">Semester: ${i+1}</th>
+                </tr>
+                <tr>
+                <th>Course Name</th>
+                <th>Course Type</th>
+                <th>Credits</th>
+                </tr>
+            `;
+            for(let j=0;j<sem_wise[i].length;j++){
+                x+=`
+                <tr>
+                   <td>
+                    ${sem_wise[i][j].cname}
+                   </td> 
+                   <td>
+                    ${sem_wise[i][j].ctype}
+                   </td> 
+                   <td>
+                    ${sem_wise[i][j].credits}
+                   </td> 
+                </tr>
+                `
+            }
+            x+=`
+            </table>
+            `;
+            globalList+= x;
+       }
+   }
+   
+   lc.innerHTML = globalList;
+   
+})();
---
+++ student/results/results.js
@@ -0,0 +1,330 @@
+let Line;
+(function(){
+    
+    let programme = "b.tech"
+    let data = {
+        "sgrades": [
+            {
+                "sroll": "18CS01001",
+                "cname": "PDS",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "ENGLISH",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "PHYSICS",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "MATHS",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 1,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "MATHS2",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "CHEMISTRY",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "ENGLISH2",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "CHEMISTRY LAB",
+                "ctype": "LAB",
+                "grade": 10,
+                "semno": 2,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "DATA STRUCTURES",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 3,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "MATHS3",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 3,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "DATA STRUCTURES LAB",
+                "ctype": "LAB",
+                "grade": 9,
+                "semno": 3,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "DISCRETE MATHS",
+                "ctype": "CORE",
+                "grade": 9,
+                "semno": 3,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "COMBINATORICS",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 4,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "ALGORITHMS",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 4,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "ALGORITHMS LAB",
+                "ctype": "LAB",
+                "grade": 9,
+                "semno": 4,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "INTRO TO ML",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 4,
+                "credits": 1
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "OPERATING SYSTEMS LAB",
+                "ctype": "LAB",
+                "grade": 10,
+                "semno": 5,
+                "credits": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "COA",
+                "ctype": "CORE",
+                "grade": 9,
+                "semno": 5,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "OPERATING SYSTEMS",
+                "ctype": "CORE",
+                "grade": 10,
+                "semno": 5,
+                "credits": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "cname": "COA LAB",
+                "ctype": "LAB",
+                "grade": 10,
+                "semno": 5,
+                "credits": 3
+            }
+        ],
+        "sgpa": [
+            {
+                "sroll": "18CS01001",
+                "gpa": 10.0,
+                "semno": 1
+            },
+            {
+                "sroll": "18CS01001",
+                "gpa": 10.0,
+                "semno": 2
+            },
+            {
+                "sroll": "18CS01001",
+                "gpa": 9.53,
+                "semno": 3
+            },
+            {
+                "sroll": "18CS01001",
+                "gpa": 9.75,
+                "semno": 4
+            },
+            {
+                "sroll": "18CS01001",
+                "gpa": 9.71,
+                "semno": 5
+            }
+        ]
+    }
+    let sgpa = data.sgpa;
+    let grades = data.sgrades;
+    let k = document.getElementById("gtables");
+    let globalTable =``;
+    let sem_wise = [];
+    for(let i=0;i<8;i++){
+        sem_wise.push([]);
+    }
+    for(let i=0;i<grades.length;i++){
+        sem_wise[grades[i].semno-1].push(grades[i]);
+    }
+    let sem_wise_grades = [];
+    for(let i=0;i<8;i++){
+        sem_wise_grades.push([]);
+    }
+    for(let i=0;i<sgpa.length;i++){
+        sem_wise_grades[sgpa[i].semno-1].push(sgpa[i].gpa);
+    }
+    let CGPA=0;
+    let counter=0;
+    let gradesForP =[];
+    for(let i=0;i<8;i++){
+        gradesForP.push(sem_wise_grades[i].length==0?0:sem_wise_grades[i][0]);
+        if(sem_wise_grades[i].length!=0){
+            CGPA+= sem_wise_grades[i][0];
+            //counter++;
+        }
+    } 
+    
+    console.log(sem_wise_grades);
+    for(let i=0;i<8;i++){
+        if(sem_wise[i].length!=0){
+            let x = `
+            <p>
+               
+            </p>
+            <p>
+               
+            </p>
+            <table class="w-100 mx-auto">
+                <tr>
+                <th class="xyz1">
+                Semester: ${i+1}
+                </th>
+                <th class="xyz1">
+                </th>
+                <th class="xyz1">
+                </th>
+                <th class="xyz1 xyz2">
+                SGPA: ${sem_wise_grades[i][0]}
+                </th>
+                </tr>
+                <tr>
+                    <th>
+                        ${"Course Name"}
+                    </th>
+                    <th>
+                        ${"Course Type"}
+                    </th>
+                    <th>
+                        ${"Credits"}
+                    </th>
+                    <th>
+                        ${"Grade"}
+                    </th>
+                </tr>
+            `
+            for(let j=0;j<sem_wise[i].length;j++){
+                x+=`
+                <tr>
+                <td>
+                    ${sem_wise[i][j].cname}
+                </td>
+                <td>
+                    ${sem_wise[i][j].ctype}
+                </td>
+                <td>
+                    ${sem_wise[i][j].credits}
+                </td>
+                <td>
+                    ${sem_wise[i][j].grade}
+                </td>
+                </tr>
+                `
+            }
+            x+=`
+            </table>
+            `
+           globalTable+=x;
+        }
+        
+    }
+    while(gradesForP.length && gradesForP[gradesForP.length-1]==0){
+        //console.log(gradesForP.length)
+        gradesForP.pop();
+    }
+    counter = gradesForP.length; 
+    CGPA = (counter==0)? 0:CGPA/counter;
+    globalTable+=`
+    <div>
+       <strong>CGPA</strong>: ${CGPA}
+    </div>
+    `
+    k.innerHTML = globalTable;
+
+    let cxt = document.getElementById("progress");
+    if(programme=="b.tech")drawLine(cxt,gradesForP,[1,2,3,4,5,6,7,8]);
+    if(programme=="m.tech")drawLine(cxt,gradesForP,[1,2,3,4]);
+})();
+function drawLine(cxt,grades,labels){
+    if(Line){
+        Line.destroy();
+    }
+    let _data = {
+        labels:labels,
+        datasets:[
+            {
+                label:'SGPA',
+                fill:false,
+                data:grades,
+                borderColor:'rgb(255, 99, 132)'
+            }
+        ]
+    }
+    let _options = {
+        
+    }
+    Line = new Chart(cxt,
+        {
+            type:'line',
+            data:_data,
+            options:_options
+        });
+        
+}
